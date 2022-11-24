78bcd17fa6c49e53452bd849375ac5d4e771424f
Walbre <brewalguyon@gmail.com>
2022-11-24 22:40:08 +0100

    avancement du script de traitement de donnees

--- traiter/traiter.py
+++ traiter/traiter.py
@@ -44,7 +44,7 @@ class data_handler:
         return self.raw
 
     
-    def get_data_in_list(self, first_line=False) -> List[List[str]]:
+    def get_data_in_list(self, first_line:bool=False) -> List[List[str]]:
         """
         Renvoie une liste a 2 dimensions qui contient les differentes donnees en string
         
@@ -62,21 +62,64 @@ class data_handler:
             List[List[str]]: la liste de liste
         """
         return self.lines[1:] if not first_line else self.line
-
-    def average_time_diff(self) -> float:
+    
+    def get_data_in_list_of_numbers(self) -> List[List[int]]:
         """
-        Calcule le temps moyen entre 2 lignes du fichier csv
+        Remove all units from the data and returning only the list of numbers (as get_data_in_list() but without units)
 
+        Returns:
+            List[List[int]]: The data once formated
+        """
+        
+        numbers : List[int] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        number : str = ""
+        data : List[List[str]] = self.get_data_in_list()
+        newList : List[List[int]] = []
+        
+        for line in range(len(data)):
+            newList.append([])
+            for element in range(len(data[line])):
+                number = ""
+                for char in range(len(data[line][element])):
+                    if data[line][element][char] in numbers:
+                        number += data[line][element][char]
+                newList[-1].append(int(number))
+        
+        return newList
+                        
+
+    def average_time_diff(self, column:int=0) -> float:
+        """
+        Calcule le temps moyen entre 2 lignes du fichier csv, la colonne contenat le temps est précisé avecle paramètre column
+
+        Args:
+            column int (default to 0): La position de la colonne indiquant le temps
+        
         Returns:
             float: Le temps moyen
         """
         
         data = self.get_data_in_list()
+        average_time : int = 0
+        
+        for i in range(1, len(data)):
+            average_time += int(data[i][column]) - int(data[i-1][column])
+        
+        return average_time / (len(data) - 1)
 
-myData = data_handler("../data.csv")
 
-print(myData.get_raw_data_in_string())
-print(myData.get_data_in_list())
+
+
+            
+
+
+if __name__ == '__main__':
+    myData = data_handler("../data.csv")
+    
+    print(myData.get_raw_data_in_string())
+    print(myData.get_data_in_list())
+    print(myData.average_time_diff())
+    print(myData.get_data_in_list_of_numbers())
                 
 
         
